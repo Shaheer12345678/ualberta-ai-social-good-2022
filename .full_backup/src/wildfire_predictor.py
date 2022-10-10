7e4bff7b8b6f083b8ef7f32e45d2f1db4ccfe88a
Shaheer Shakir <shaheershakir123@gmail.com>
2022-10-10 16:00:00 -0700

    Initial scaffold and README

---
+++ .full_backup/src/wildfire_predictor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import argparse, numpy as np, pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, classification_report
+from sklearn.ensemble import RandomForestClassifier
+np.random.seed(42)
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# UAlberta AI for Social Good Hackathon 2022
+
+**Organizers:** UAlberta AI Club × Amii (Alberta Machine Intelligence Institute)  
