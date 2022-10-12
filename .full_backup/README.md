494efcd37b71cbd878f6028bf42cf18217c8a8ce
Shaheer Shakir <shaheershakir123@gmail.com>
2022-10-11 22:24:00 -0700

    Add basic source files

--- .full_backup/README.md
+++ .full_backup/README.md
@@ -1,3 +1,6 @@
 # UAlberta AI for Social Good Hackathon 2022
 
-**Organizers:** UAlberta AI Club × Amii (Alberta Machine Intelligence Institute)  
+**Organizers:** UAlberta AI Club × Amii (Alberta Machine Intelligence Institute)  
+**Submission:** Google Forms + shared links (no public gallery).
+
+This folder provides a small, reproducible **WildfirePredictor** example that trains on synthetic features and
--- src/wildfire_predictor.py
+++ src/wildfire_predictor.py
@@ -4,4 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, classification_report
 from sklearn.ensemble import RandomForestClassifier
 np.random.seed(42)
-
+
+def synthesize(n=2000):
+    # Features: temp, humidity, wind, vegetation_dryness
+    temp = np.random.normal(25, 7, n)
+    humidity = np.random.uniform(10, 80, n)
+    wind = np.random.normal(15, 6, n)
+    dryness = np.random.uniform(0, 1, n)
+    # Risk label (synthetic rule + noise)
