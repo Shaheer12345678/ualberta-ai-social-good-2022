090d274035445bb153fdb257d7de6128544482e6
Shaheer Shakir <shaheershakir123@gmail.com>
2022-10-13 04:48:00 -0700

    Implement core logic

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 **Organizers:** UAlberta AI Club × Amii (Alberta Machine Intelligence Institute)  
 **Submission:** Google Forms + shared links (no public gallery).
 
-This folder provides a small, reproducible **WildfirePredictor** example that trains on synthetic features and
+This folder provides a small, reproducible **WildfirePredictor** example that trains on synthetic features and
+outputs a simple risk score. It demonstrates ML workflow hygiene (reproducibility, metrics, CLI).
+
+Run:
--- src/wildfire_predictor.py
+++ src/wildfire_predictor.py
@@ -11,4 +11,10 @@ def synthesize(n=2000):
     humidity = np.random.uniform(10, 80, n)
     wind = np.random.normal(15, 6, n)
     dryness = np.random.uniform(0, 1, n)
-    # Risk label (synthetic rule + noise)
+    # Risk label (synthetic rule + noise)
+    logit = 0.08*temp - 0.04*humidity + 0.09*wind + 2.0*dryness - 4.0
+    prob = 1/(1+np.exp(-logit))
+    y = (prob > 0.5).astype(int)
+    X = pd.DataFrame({"temp":temp,"humidity":humidity,"wind":wind,"dryness":dryness})
+    return X, y
+
